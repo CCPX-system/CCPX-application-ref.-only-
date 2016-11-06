65c2d7d62134ad9b386e47a1be19ade5f03a25ac
baagiiSW <sw09d051@csms.edu.mn>
2016-11-06 20:32:05 +0800

    Platform function Controller

---
+++ src/controller/PlatformController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import java.util.List;
+
+import model.Notification;
+
+import model.Offer;
+import model.Request;
+import model.user_to_seller;
+import dao.PlatformDao;
+import dao.PlatformDaoImp;
+
+
+public class PlatformController {
+	
+ public List showLatestTransaction(){
+	return null;
+	 }
+ 
+}
