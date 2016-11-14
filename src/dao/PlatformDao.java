a5a15803a4532b2e350e575de32ea9d8a4c4c560
baagiiSW <sw09d051@csms.edu.mn>
2016-11-15 00:10:01 +0800

    Sprint2

--- src/dao/PlatformDao.java
+++ src/dao/PlatformDao.java
@@ -1,15 +1,19 @@
 package dao;
 
+
+
+import java.util.List;
+
 import model.Offer;
 import model.Request;
 
 public interface PlatformDao {
 
-	public Request showLatestTransaction(Integer sellerFrom, Integer sellerto);  
+	public List<Request> showLatestTransaction(Integer sellerFrom, Integer sellerto);  
 	
-	public Offer searchExcahnge(Integer sellerFrom, Integer sellerTo, Integer pointsFrom, Integer pointsToMin);
+	public List<Offer> searchExcahnge(Integer sellerFrom, Integer sellerTo, Integer pointsFrom, Integer pointsToMin);
 	
-	public Offer showRecommendationList(String sellerFrom, String sellerTo, String pointsFrom, String pointsToMin);
+	public List<Offer> showRecommendationList(Integer sellerFrom, Integer sellerTo, Integer pointsFrom, Integer pointsToMin);
 	
 	
 }
--- src/dao/PlatformDaoImp.java
+++ src/dao/PlatformDaoImp.java
@@ -1,5 +1,8 @@
 package dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import model.Offer;
@@ -9,6 +12,7 @@ import model.Request;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+
 
 public class PlatformDaoImp implements PlatformDao 
 {
@@ -30,31 +34,53 @@ public class PlatformDaoImp implements PlatformDao
 
 
 	@Override
-	public Offer searchExcahnge(Integer sellerFrom, Integer sellerTo,
+	public List<Offer> searchExcahnge(Integer sellerFrom, Integer sellerTo,
 			Integer pointsFrom, Integer pointsToMin) {
-		String sql = "";
+		List<Offer> list = new ArrayList<Offer>(); 
+		String sql = "from Offer where pointsFrom>= :pointsToMin and pointsToMin<= :pointsFrom "
+				+ "and sellerFrom =:sellerTo and sellerTo =:sellerFrom and status= :status";
 		Query query = getSession().createQuery(sql);
-		query.setInteger("e", sellerFrom);
-		query.setInteger("d", sellerTo);
-		query.setInteger("", pointsFrom);
-		query.setInteger("", pointsToMin);
+		query.setInteger("sellerFrom", sellerFrom);
+		query.setInteger("sellerTo", sellerTo);
+		query.setInteger("pointsFrom", pointsFrom);
+		query.setInteger("pointsToMin", pointsToMin);
 		query.setInteger("status", 1);
-		Offer offer = (Offer) query.list();
-		return offer;
+		list = query.list();
+		return list;
 		
 	}
-
+	
 	@Override
-	public Offer showRecommendationList(String sellerFrom, String sellerTo,
-			String pointsFrom, String pointsToMin) {
+	public List<Request> showLatestTransaction(Integer sellerFrom, Integer sellerTo) {
 		// TODO Auto-generated method stub
-		return null;
+		String sql = "from request where " + "(SELLER_ID_FROM =? and SELLER_ID_TO =? "
+				+ "and status = 'accepted')" + " or (SELLER_ID_TO =? and SELLER_ID_FROM =?"
+				+ "and status = 'accepted')" + " order by UPDATE_TIME desc limit 5";
+		Query query = getSession().createQuery(sql);
+		query.setInteger(0, sellerFrom);
+		query.setInteger(1, sellerTo);
+		query.setInteger(2, sellerFrom);
+		query.setInteger(3, sellerTo);
+		List<Request> requests = query.list();
+		return requests;
 	}
 
-
 	@Override
-	public Request showLatestTransaction(Integer sellerFrom, Integer sellerto) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public List<Offer> showRecommendationList(Integer sellerFrom, Integer sellerTo,
+			Integer pointsFrom, Integer pointsToMin) {
+			List<Offer> list = new ArrayList<Offer>(); 
+			String hql = "from Offer where pointsFrom>= :pointsToMin and pointsToMin<= :pointsFrom "
+					+ "and sellerFrom =:sellerTo and sellerTo =:sellerFrom and status= :2";
+			Query query = getSession().createQuery(hql);
+			query.setInteger("sellerFrom", sellerFrom);
+			query.setInteger("sellerTo", sellerTo);
+			query.setInteger("pointsFrom", pointsFrom);
+			query.setInteger("pointsToMin", pointsToMin);
+			Offer offer = (Offer) query.uniqueResult();
+			list.add(offer);
+			return list;
+		}
+		
+
+	
 }
